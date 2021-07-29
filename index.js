c6b911f430221654ea2b5977997523add83f410c
Ariel Mulet <amulet@unitedvirtualities.com>
2021-07-29 12:58:36 -0300

    update PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import routes from './routes'
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.use('/api', routes)
 
 app.listen(PORT, () => {
